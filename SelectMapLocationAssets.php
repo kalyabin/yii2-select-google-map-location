00bd5d239003f8220483d248d0edec1a66766890
elfarqy <alvianthelfarqy@gmail.com>
2019-06-11 10:57:25 +0700

    remove sensor in js
    https://developers.google.com/maps/documentation/javascript/error-messages#SensorNotRequired

--- SelectMapLocationAssets.php
+++ SelectMapLocationAssets.php
@@ -34,7 +34,7 @@ class SelectMapLocationAssets extends AssetBundle
     public static function register($view)
     {
         /* @var $view \yii\web\View */
-        $view->registerJsFile('https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=true&key=' . self::$googleMapApiKey);
+        $view->registerJsFile('https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=' . self::$googleMapApiKey);
         return parent::register($view);
     }
 }
